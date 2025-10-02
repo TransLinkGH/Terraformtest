771f9f2230460cd59ab46580f876dbae71c659db
TransLinkGH <g_harjit@hotmail.com>
2025-10-02 11:24:33 -0700

    terraform resource group creation

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform_training_resource_group"
+  location = "westus2"
+
+}
+
